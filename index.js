3f30f7440ef3750ccf191606a711b1c7172475a9
maxigong <maxi@nolgong.com>
2023-01-02 12:07:07 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import { CSS3DObject } from "./libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js";
+
+gsap.set(".link", { opacity: 0, zIndex: -1 });
+let tl = gsap.timeline();
+tl.from(".card-header", { opacity: 0, yPercent: -10 })
+    .to("h1", {
+        duration: 1,
+        text: {
+            value: "nolgong",
+        },
+    })
+    .to(".link", { opacity: 1, y: 220, stagger: 0.3 })
+    .pause();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const start = async () => {
+        const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+            container: document.body,
+            imageTargetSrc: "./targetsTwo.mind",
+        });
+
+        const { renderer, camera, cssScene, cssRenderer } = mindarThree;
+        const mainContainer = new CSS3DObject(
+            document.querySelector("#cardMain")
+        );
+
+        mainContainer.userData.clickable = true;
+        const cssAnchor = mindarThree.addCSSAnchor(0);
+        cssAnchor.group.add(mainContainer);
+
+        cssAnchor.onTargetFound = () => {
+            tl.play();
+        };
+        cssAnchor.onTargetLost = () => {
+            tl.reverse();
+        };
+
+        await mindarThree.start();
+
+        renderer.setAnimationLoop(() => {
+            cssRenderer.render(cssScene, camera);
+        });
+    };
+    start();
+});
